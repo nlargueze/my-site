5492355e0d0bd83b11e41ac8eef7990aa0edaec1
Nicolas Largueze <nick@nicklabs.io>
2024-08-29 18:45:16 +0200

    Added

--- docs/_app/immutable/chunks/paths.Bon0hoES.js
+++
@@ -1,1 +0,0 @@
-var s;const t=((s=globalThis.__sveltekit_1ujy1h2)==null?void 0:s.base)??"/my-site";var e;const a=((e=globalThis.__sveltekit_1ujy1h2)==null?void 0:e.assets)??t;export{a,t as b};
--- docs/_app/immutable/entry/start.CgibA_K2.js
+++
@@ -1,1 +0,0 @@
-import{a as t}from"../chunks/entry.CDE2f4y7.js";export{t as start};
